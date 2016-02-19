1722f7314b8c288da668cac8835e6c1b87d6367f
Jason Nallie <jnallie@gmail.com>
2016-02-18 23:48:45 -0600

    edit gitignore to inline css styles for timeline  -hook up webfonts  -edit type styles

--- templates/code-header.php
+++ templates/code-header.php
@@ -7,6 +7,7 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
+<script type="text/javascript" src="fonts/campton/campton.js"></script>
 <script src="dist/js/vendor/modernizr.min.js"></script> <!-- Modernizr -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 <!-- <link rel="stylesheet" href="dist/css/main.min.css"> -->
